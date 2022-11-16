14b4085d75e8d179f98a4709d2495cf88f1a646f
prakashpatel1 <prakashbpatel02@gmail.com>
2022-11-17 00:45:48 +0530

    animation

--- header.php
+++ header.php
@@ -10,17 +10,20 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" rel="stylesheet" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/Css/style.css">
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link rel="stylesheet" href="./assets/Css/style.css">
 </head>
 
-<body>  
+<body class="index-page">  
+    <include src="./assets/header.html"></include>
     <section class="header">
         <div class="container-fluid">
             <div class="row align-items-baseline">
                 <div class="col-lg-3 col-md-6 col-6">
                     <div class="header_logo">
-                        <a href="../work/index.html"><img src="./assets/Images/logo.svg" width="100%"/></a>
+                        <a href="./index.html"><img src="./assets/Images/logo.svg" width="100%"/></a>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-6 d-flex justify-content-end">
@@ -31,36 +34,39 @@
                             </button>
                             <div class="navmenu">
                                 <div class="mobile-menu">
-                                    <div class="top-em-link">
+                                    <!-- <div class="top-em-link">
                                         <a href="mailto:contact@invints.com" class="text-gray d-block">contact@invints.com</a>
                                         <a href="tel:+91 99786 01134" class="text-white">+91 99786 01134</a>
-                                    </div>
+                                    </div> -->
+                                    <a href="./index.html"><img src="./assets/Images/logo.svg" width="100%"/></a>
                                    <!--  <div class="header_logo">
                                         <a href="../work/index.html"><img src="./assets/Images/logo.svg" width="100%"/></a>
                                     </div> -->
                                     <span class="navigation-body-close-button text-danger">✕</span>
+                                    
                                 </div>
                                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                     <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page" href="../work/index.html">Home</a>
+                                        <a class="nav-link active" aria-current="page" href="./index.html">Home</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="../work/aboutus.html">About Us</a>
+                                        <a class="nav-link" href="./aboutus.html">About Us</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link " href="../work/portfolios.html">Portfolio</a>
+                                        <a class="nav-link " href="./portfolios.html">Portfolio</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link " href="../work/career.html">Career</a>
+                                        <a class="nav-link " href="./career.html">Career <span class="me-3">We’r Hiring!</span></a>
                                     </li>
-                                    <li class="nav-item">
+                                    
+                                    <!-- <li class="nav-item">
                                         <a class="nav-link " href="#">We’r Hiring!</a>
+                                    </li> -->
+                                    <li class="nav-item">
+                                        <a class="nav-link " href="./contactus.html">Contact</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link " href="../work/contactus.html">Contact</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="../work/team.html">Team</a>
+                                        <a class="nav-link" href="./team.html">Team</a>
                                     </li> 
                                     <!-- <div class="buttonBox position-relative d-inline-flex">
                                         <button class="btn-style"><img src="./assets/Images/arrow_right.png"> Get Started </span></button>
@@ -68,44 +74,37 @@
                                         <div class="border-class"></div>
                                     </div>                                    -->
                                 </ul>
-                                <div class="nav-head-bottom">
+                                <!-- <div class="nav-head-bottom">
                                     <a href="">Dribbble</a>
                                     <a href="">Behance</a>
                                     <a href="">Instagram</a>
                                     <a href="">uplabs</a>
-                                </div>
+                                </div> -->
                             </div>
                         </div>
                     </nav>
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-4 col-6 d-flex justify-content-end">
                     <div class="header_btn">
-                        <button class="btn"><a href="../work/contactus.html">Let’s Talk</a><img src="./assets/Images/right_arrow.png" class="ms-3" width="14px"></button>
+                        <button class="btn"><a href="./contactus.html">Let’s Talk</a><img src="./assets/Images/right_arrow.png" class="ms-3" width="14px"></button>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 
+    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
     <script>
+
         jQuery(document).ready(function(){
-            jQuery('.slider').slick({
-                infinite: true,
-                dots: true,
-                arrows: true,
-                autoplay: true,
-                autoplaySpeed: 3000,
-                fade: true,
-                fadeSpeed: 1000
-            });
+       
 
             /* Header Responsive */
 
-             /* Header Responsive */
-
-             jQuery('.navbar-toggler-icon').click(function(){
+            jQuery('.navbar-toggler-icon').click(function(){
                 jQuery('.navmenu').addClass("active");
                 jQuery('.navmenu').removeClass("remove");
             });
@@ -116,9 +115,11 @@
 
             jQuery(".navmenu .nav-item .nav-link").click(function(){
                 jQuery('.navmenu .nav-item .nav-link').removeClass('active');
-                 jQuery(this).addClass('active');
+                jQuery(this).addClass('active');
             });
         });
+
+        AOS.init();
     
     </script>
 </body>
